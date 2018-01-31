738397a5dba6fc96a4dafcaf5f3a57579cbd6a86
Asenchik Anastasiya <n-asenchik@yandex.ru>
2018-01-31 22:21:19 +0300

    some renames

--- js/setup.js
+++ js/setup.js
@@ -2,9 +2,9 @@
 
 var WIZARD_FIRSTNAMES = ['Иван', 'Хуан Себастьян', 'Мария', 'Кристоф', 'Виктор', 'Юлия', 'Люпита', 'Вашингтон'];
 var WIZARD_LASTNAMES = ['да Марья', 'Верон', 'Мирабелла', 'Вальц', 'Онопко', 'Топольницкая', 'Нионго', 'Ирвинг'];
-var WIZARD_COAT_COLOR = ['rgb(101, 137, 164)', 'rgb(241, 43, 107)', 'rgb(146, 100, 161)', 'rgb(56, 159, 117)', 'rgb(215, 210, 55)', 'rgb(0, 0, 0)'];
-var WIZARD_EYES_COLOR = ['black', 'red', 'blue', 'yellow', 'green'];
-var quantityWizard = 4;
+var WIZARD_COAT_COLORS = ['rgb(101, 137, 164)', 'rgb(241, 43, 107)', 'rgb(146, 100, 161)', 'rgb(56, 159, 117)', 'rgb(215, 210, 55)', 'rgb(0, 0, 0)'];
+var WIZARD_EYES_COLORS = ['black', 'red', 'blue', 'yellow', 'green'];
+var QUANTITY_WIZARD = 4;
 var similarWizardTemplate = document.querySelector('#similar-wizard-template').content;
 
 document.querySelector('.setup').classList.remove('hidden');
@@ -17,14 +17,14 @@ var getWizardProperties = function (quantity) {
   for (var i = 0; i < quantity; i++) {
     properties.push({
       name: WIZARD_FIRSTNAMES[Math.floor(Math.random() * WIZARD_FIRSTNAMES.length)] + ' ' + WIZARD_LASTNAMES[Math.floor(Math.random() * WIZARD_LASTNAMES.length)],
-      coatColor: WIZARD_COAT_COLOR[Math.floor(Math.random() * WIZARD_COAT_COLOR.length)],
-      eyesColor: WIZARD_EYES_COLOR[Math.floor(Math.random() * WIZARD_EYES_COLOR.length)]
+      coatColor: WIZARD_COAT_COLORS[Math.floor(Math.random() * WIZARD_COAT_COLORS.length)],
+      eyesColor: WIZARD_EYES_COLORS[Math.floor(Math.random() * WIZARD_EYES_COLORS.length)]
     });
   }
   return properties;
 };
 
-var wizards = getWizardProperties(quantityWizard);
+var wizards = getWizardProperties(QUANTITY_WIZARD);
 
 // генерация блока волшебника с помощью шаблона
 
@@ -39,7 +39,7 @@ var renderWizard = function (wizard) {
 // добавление волшебников в разметку по шаблону
 
 var fragment = document.createDocumentFragment();
-for (var i = 0; i < quantityWizard; i++) {
+for (var i = 0; i < QUANTITY_WIZARD; i++) {
   fragment.appendChild(renderWizard(wizards[i]));
 }
 document.querySelector('.setup-similar-list').appendChild(fragment);
